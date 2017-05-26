8e21a7a2adba4e4765e0b3185375ce673325379c
Jordan Last <jordan.michael.last@gmail.com>
2017-05-25 21:31:45 -0600

    initial test works with attributes!

--- data-bound.js
+++ data-bound.js
@@ -1,1 +1,9 @@
+const DataBound = function(strings) {
+    console.log(arguments);
 
+    const dataBoundArguments = arguments;
+
+    return strings.reduce((result, string, index) => {
+        return result + string + (dataBoundArguments[index + 1] ? dataBoundArguments[index + 1] : '');
+    }, '');
+};
