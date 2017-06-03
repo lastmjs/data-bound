f1948626383791889f2d4b4868296ccfef4166fa
Jordan Last <jordan.michael.last@gmail.com>
2017-06-02 20:46:38 -0600

    arbitrary text locations working well

--- data-bound.js
+++ data-bound.js
@@ -2,15 +2,14 @@
 // create a string with all of the expressions replaced with uuids
 // for each element, loop through all attributes. If the attribute value contains the uuid, store that element and that attribute name to the mapping
 
-let dataBindings;
-
+//TODO I need to make the function work for multiple calls from multiple components
 const DataBound = function(strings) {
+    let dataBindings;
     const originalArguments = arguments;
     return function(component) {
         const expressions = Array.from(originalArguments).slice(1);
 
         if (!dataBindings) {
-            console.log('if')
             const elementInfos = expressions.map((expression) => {
                 const uuid = createUUID();
                 return {
@@ -30,39 +29,55 @@ const DataBound = function(strings) {
             }, '');
 
             dataBindings = elementInfos.map((elementInfo) => {
-                const {element, attributeName} = Array.from(component.children).reduce((result, child) => {
-                    const {uuidPresent, attributeName} = Array.from(child.attributes).reduce((result, attribute) => {
+                const {
+                    element,
+                    attributeName,
+                    textNode,
+                    eventName
+                } = Array.from(getAllChildren(component)).reduce((result, child) => {
+                    const {
+                        attributeName
+                    } = Array.from(child.attributes).reduce((result, attribute) => {
                         const uuidPresent = attribute.value.includes(elementInfo.uuid);
                         const attributeName = attribute.name;
 
-                        console.dir(attribute)
-
                         return Object.assign({}, result, {
-                            uuidPresent: uuidPresent ? uuidPresent : result.uuidPresent,
                             attributeName: uuidPresent ? attributeName : result.attributeName
                         });
                     }, {
-                        uuidPresent: null,
                         attributeName: null
                     });
 
+                    // We must find out if any childNodes are text nodes, and bind to the textContent property of the childNode
+                    const {textNode} = Array.from(child.childNodes).reduce((result, childNode) => {
+                        return Object.assign({}, result, {
+                            textNode: childNode.nodeName === '#text' && childNode.textContent.includes(elementInfo.uuid) ? childNode : result.textNode
+                        });
+                    }, {
+                        textNode: null
+                    });
+
                     return Object.assign({}, result, {
-                        element: uuidPresent ? child : result.element,
-                        attributeName: uuidPresent ? attributeName : result.attributeName
+                        element: attributeName || textNode ? child : result.element,
+                        attributeName: attributeName || result.attributeName,
+                        textNode: textNode || result.textNode
                     });
                 }, {
                     element: null,
-                    attributeName: null
+                    attributeName: null,
+                    textNode: null,
+                    eventName: null
                 });
 
                 return Object.assign({}, elementInfo, {
                     element,
-                    attributeName
+                    attributeName,
+                    textNode,
+                    eventName
                 });
             });
         }
         else {
-            console.log('else')
             dataBindings = dataBindings.map((dataBinding, index) => {
                 return Object.assign({}, dataBinding, {
                     expression: expressions[index]
@@ -70,25 +85,50 @@ const DataBound = function(strings) {
             });
         }
 
-        console.log('about to set properties again');
-
         // this is where the actual data binding occurs
-        dataBindings.forEach((dataBinding) => {
-            dataBinding.element.setAttribute(dataBinding.attributeName, dataBinding.expression); // we set the attribute to get rid of the uuid that we set earlier
-            // dataBinding.element[dataBinding.attributeName] = dataBinding.expression; // now we set the property
-            // console.log(dataBinding.element);
-            // setTimeout(() => {
-            //     dataBinding.element[dataBinding.attributeName] = dataBinding.expression; // now we set the property
-            // }, 50);
-            console.log(dataBinding.attributeName);
-            console.log(dataBinding.element.attributeName);
-            console.log(dataBinding.element);
-            dataBinding.element[dataBinding.attributeName] = dataBinding.expression; // now we set the property
-            dataBinding.element[dataBinding.attributeName] = dataBinding.expression; // now we set the property
-            //TODO the property setters are not firing correctly it seems when I set the properties here
+        dataBindings.forEach(async (dataBinding) => {
+            //TODO how to figure out a better way to handle passing in non-custom-element names
+            try {
+                await window.customElements.whenDefined(dataBinding.element.localName);
+            }
+            catch(error) {
+                // console.log(error)
+            }
+
+            switch(dataBinding.attributeName) {
+                case 'style': {
+                    dataBinding.element.setAttribute(dataBinding.attributeName, dataBinding.element.getAttribute('style').replace(dataBinding.uuid, dataBinding.expression));
+                    break;
+                }
+                case 'class': {
+                    dataBinding.element.setAttribute(dataBinding.attributeName, dataBinding.element.getAttribute('class').replace(dataBinding.uuid, dataBinding.expression));
+                    break;
+                }
+                default: {
+                    if (dataBinding.textNode) {
+                        dataBinding.textNode.textContent = dataBinding.textNode.textContent.replace(dataBinding.uuid, dataBinding.expression);
+                    }
+                    else {
+                        dataBinding.element.setAttribute(dataBinding.attributeName, dataBinding.expression); // we set the attribute to get rid of the uuid that we set earlier
+                        dataBinding.element[dataBinding.attributeName] = dataBinding.expression; // now we set the property
+                    }
+
+                    break;
+                }
+            }
         });
     };
 };
+
+//TODO make this more functional
+function getAllChildren(element) {
+    const immediateChildren = Array.from(element.children);
+    let allChildren = [...immediateChildren];
+    immediateChildren.forEach((child) => {
+        allChildren.push(...getAllChildren(child));
+    });
+    return allChildren;
+}
 
 function createUUID() {
     //From persistence.js; Copyright (c) 2010 Zef Hemel <zef@zef.me> * * Permission is hereby granted, free of charge, to any person * obtaining a copy of this software and associated documentation * files (the "Software"), to deal in the Software without * restriction, including without limitation the rights to use, * copy, modify, merge, publish, distribute, sublicense, and/or sell * copies of the Software, and to permit persons to whom the * Software is furnished to do so, subject to the following * conditions: * * The above copyright notice and this permission notice shall be * included in all copies or substantial portions of the Software. * * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR * OTHER DEALINGS IN THE SOFTWARE.
